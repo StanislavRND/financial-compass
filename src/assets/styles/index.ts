aa9176f757ba64b587c55b9d30bf97ca8eaa278d
StanislavRND-2 <nenotice65@gmail.com>
2025-05-05 22:26:26 +0300

    Delete src

--- src/assets/styles/index.ts
+++
@@ -1,3 +0,0 @@
-import BaseStyle from './base.module.scss'
-
-export { BaseStyle }
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { AppRouter, Providers } from './application'
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root')!).render(
-  <Providers>
-    <AppRouter />
-  </Providers>,
-)
